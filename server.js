d10f2f93deca5be49e41c0ec14048c0d91e4a989
DuNT <ntd160296@gmail.com>
2022-08-20 14:30:45 +0700

    remove

--- server.js
+++ server.js
@@ -1,45 +1,12 @@
-const http = require('http');
-const file = require('fs');
+var express = require('express');
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...');
-    // res.setHeader('Content-Type', 'text/html');
-    // res.writeHead(200, {'ContentType' : 'aplication/json'});
-    // var doituong = {
-    //     ten: 'long',
-    //     nghenghiep: 'develop',
-    //     quequan:'nam dinh'
-    // };
-    // var docLuong = file.createReadStream(__dirname + '/index.html', 'utf-8');
-    // docLuong.pipe(res);
+var app = express();
 
-    // res.end(JSON.stringify(doituong));
-
-    //bai2;
-    console.log('nguoi dung yeu cau link: ' + req.url);
-    if (req.url === '/' || req.url === '/home') {
-        bai2(req, res, 'text/html', 'index.html');
-    }
-    else if (req.url === '/login') {
-        bai2(req, res, 'text/html', 'login.html');
-    }
-    else if (req.url === '/register') {
-        // bai2(req, res, 'aplication/json', 'login.html');
-        var doiTuong = {
-            ten: 'long',
-            quequan: 'nam dinh',
-            nghenghiep: 'dong nat',
-            tuoi: '100'
-        };
-        res.end(JSON.stringify(doiTuong));
-    }
+app.get('/', function (req, res) {
+    res.send('This is home!');
+});
+app.get('/contact/:userId/send/:content', function (req, res) {
+    res.send('Custom with id is ' + req.params.userId + ' want to said: ' + req.params.content);
 });
 
-function bai2(req, res, contentType, page) {
-    res.writeHead(200, { 'ContentType': contentType });
-    file.createReadStream(__dirname + '/' + page, 'utf-8').pipe(res);
-}
-
-server.listen(8808, 'localhost', () => {
-    console.log('Node.JS server is running on port: 8808');
-});
+app.listen(8000);
