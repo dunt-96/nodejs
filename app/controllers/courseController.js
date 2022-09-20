4f3e27d9078fc7712bed3085bee973b2e58c902c
DuNT <ntd160296@gmail.com>
2022-09-20 20:16:00 +0700

    commit

--- app/controllers/courseController.js
+++ app/controllers/courseController.js
@@ -1,102 +1,111 @@
-
-
-
 class CourseController {
-    getCourseById(req, res) {
-        const course = courses.find(course => course.id === parseInt(req.params.id));
+  getCourseById(req, res) {
+    const course = courses.find((course) => course.id === parseInt(req.params.id));
 
-        if (!course) res.status(404).send('Id ko ton tai');
+    if (!course) res.status(404).send('Id ko ton tai');
 
-        return res.send(JSON.stringify({
-            status: true,
-            data: course
-        }));
+    return res.send(
+      JSON.stringify({
+        status: true,
+        data: course,
+      })
+    );
+  }
 
-    };
+  getAllCourse(req, res) {
+    return res.send(
+      JSON.stringify({
+        status: true,
+        data: courses,
+      })
+    );
+  }
 
-    getAllCourse(req, res) {
+  deleteCourse(req, res) {
+    var course = courses.find((e) => e.id === parseInt(req.params.id));
+    console.log(course);
 
-        return res.send(JSON.stringify({
-            status: true,
-            data: courses
-        }));
-    }
-    
-    deleteCourse(req, res) {
-        var course = courses.find(e => e.id === parseInt(req.params.id));
-        console.log(course);
+    if (!course)
+      res.send(
+        JSON.stringify({
+          status: false,
+          notice: 'Xoa that bai',
+          data: courses,
+        })
+      );
 
-        if (!course) res.send(JSON.stringify({
-            status: false,
-            notice: "Xoa that bai",
-            data: courses
-        }));
+    // courses = courses.filter(e => {
+    //     return e.id != parseInt(req.params.id);
+    // });
+    const idx = courses.indexOf(course);
+    courses.splice(idx, 1);
 
-        // courses = courses.filter(e => {
-        //     return e.id != parseInt(req.params.id);
-        // });
-        const idx = courses.indexOf(course);
-        courses.splice(idx, 1);
+    return res.send(
+      JSON.stringify({
+        status: true,
+        notice: 'Xoa thanh cong',
+        data: courses,
+      })
+    );
+  }
+  editCourse(req, res) {
+    const course = req.body;
 
-        return res.send(JSON.stringify({
-            status: true,
-            notice: "Xoa thanh cong",
-            data: courses
-        }));
-    }
-    editCourse(req, res) {
-        const course = req.body;
+    console.log(course);
 
-        console.log(course);
+    if (!course)
+      res.send(
+        JSON.stringify({
+          status: false,
+          notice: 'Sua that bai',
+        })
+      );
 
-        if (!course) res.send(JSON.stringify({
-            status: false,
-            notice: "Sua that bai"
-        }));
+    const editCourse = courses.find((e) => e.id === course['id']);
 
-        const editCourse = courses.find(e => e.id === course['id']);
+    courses[courses.indexOf(editCourse)] = course;
 
-        courses[courses.indexOf(editCourse)] = course;
+    return res.send(
+      JSON.stringify({
+        status: true,
+        notice: 'Sua thanh cong',
+        data: courses,
+      })
+    );
+  }
 
-        return res.send(JSON.stringify({
-            status: true,
-            notice: "Sua thanh cong",
-            data: courses
-        }));
-    };
+  addCourse(req, res) {
+    console.log(req.body);
+    const course = req.body;
+    if (!course) return;
 
-    addCourse(req, res) {
-        console.log(req.body);
-        const course = req.body;
-        if (!course) return;
+    courses.push(course);
 
-        courses.push(course);
-
-        return res.send(JSON.stringify({
-            status: true,
-            notice: "Ban da them thanh cong",
-            data: courses
-        }))
-    };
-
+    return res.send(
+      JSON.stringify({
+        status: true,
+        notice: 'Ban da them thanh cong',
+        data: courses,
+      })
+    );
+  }
 }
-var courses =
-    [
-        {
-            id: 1,
-            name: 'java',
-        },
-        {
-            id: 2,
-            name: 'python',
-        },
-        {
-            id: 3,
-            name: 'flutter',
-        },
-        {
-            id: 4,
-            name: 'ios',
-        }
-    ]
-module.exports = new CourseController;
+var courses = [
+  {
+    id: 1,
+    name: 'java',
+  },
+  {
+    id: 2,
+    name: 'python',
+  },
+  {
+    id: 3,
+    name: 'flutter',
+  },
+  {
+    id: 4,
+    name: 'ios',
+  },
+];
+module.exports = new CourseController();
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,149 +1,161 @@
-const db = require('../models/index')
-const userService = require('../services/userService')
+const db = require("../models/index");
+const userService = require("../services/userService");
 class UserController {
-    async getAllUser(req, res) {
-        await userService.getAllUser().then((data) => {
-            return res.status(200).json({
+  async getAllUser(req, res) {
+    await userService.getAllUser().then((data) => {
+      return res.status(200).json({
+        status: true,
+        notice: "Success",
+        data: data,
+      });
+    });
+  }
+
+  async deleteUser(req, res) {
+    await userService
+      .findUserById(parseInt(req.params.id))
+      .then(async (result) => {
+        if (result.dataValues != null) {
+          await userService.deleteUser(parseInt(req.params.id)).then((data) => {
+            if (data) {
+              return res.status(200).json({
                 status: true,
-                notice: "Success",
-                data: data
-            });
-        });
-    }
-
-    async deleteUser(req, res) {
-        await userService.findUserById(parseInt(req.params.id)).then(async (result) => {
-            if (result.dataValues != null) {
-                await userService.deleteUser(parseInt(req.params.id)).then((data) => {
-                    if (data) {
-                        return res.status(200).json({
-                            status: true,
-                            notice: "Xoa nguoi dung thanh cong",
-                            data: data
-                        })
-                    }
-                });
+                notice: "Xoa nguoi dung thanh cong",
+                data: data,
+              });
             }
-        }).catch((err) => {
-            return res.status(200).json({
-                status: false,
-                notice: "Xoa nguoi dung that bai",
-            });
+          });
+        }
+      })
+      .catch((err) => {
+        return res.status(200).json({
+          status: false,
+          notice: "Xoa nguoi dung that bai",
         });
-    };
+      });
+  }
 
-    async deleteAllUser(req, res) {
-        await userService.deleteAllUser().then((result) => {
-            if (result == 0) {
-                return res.status(200).json({
-                    status: true,
-                    notice: "Xoa nguoi dung thanh cong",
-                });
-            }
-        }).catch((err) => {
-            return res.status(200).json({
-                status: false,
-                notice: "Xoa nguoi dung that bai",
-            });
-        });
-    }
-
-    async addUser(req, res) {
-        await userService.checkIsExistUser(req.body).then(async (user) => {
-            if (user.length != 0) {
-                return res.status(200).json({
-                    status: false,
-                    notice: "Email da bi trung",
-                    data: user
-                });
-            }
-
-            let result = await userService.createNewUser(req.body);
-
-            if (result.dataValues != null) {
-                return res.send(JSON.stringify({
-                    status: true,
-                    notice: "Them nguoi dung thanh cong",
-                    data: result
-                }));
-            }
-            return res.send(JSON.stringify({
-                status: false,
-                notice: "Them nguoi dung that bai",
-            }));
-        });
-    }
-
-    async getUserById(req, res) {
-        const user = db.User.findAll({
-            where: {
-                id: parseInt(req.params.id)
-            }
-        });
-
-
-        return res.send(JSON.stringify({
+  async deleteAllUser(req, res) {
+    await userService
+      .deleteAllUser()
+      .then((result) => {
+        if (result == 0) {
+          return res.status(200).json({
             status: true,
-            notice: "Success",
-            data: user
-        }));
-    }
-
-    async editUser(req, res) {
-        await userService.editUser(req.body, req.params.id).then((data) => {
-            return res.status(200).json({
-                status: true,
-                notice: "Sua thanh cong",
-                data: data
-            });
+            notice: "Xoa nguoi dung thanh cong",
+          });
+        }
+      })
+      .catch((err) => {
+        return res.status(200).json({
+          status: false,
+          notice: "Xoa nguoi dung that bai",
         });
+      });
+  }
+
+  async addUser(req, res) {
+    await userService.checkIsExistUser(req.body).then(async (user) => {
+      if (user.length != 0) {
+        return res.status(200).json({
+          status: false,
+          notice: "Email da bi trung",
+          data: user,
+        });
+      }
+
+      let result = await userService.createNewUser(req.body);
+
+      if (result.dataValues != null) {
+        return res.send(
+          JSON.stringify({
+            status: true,
+            notice: "Them nguoi dung thanh cong",
+            data: result,
+          })
+        );
+      }
+      return res.send(
+        JSON.stringify({
+          status: false,
+          notice: "Them nguoi dung that bai",
+        })
+      );
+    });
+  }
+
+  async getUserById(req, res) {
+    const user = db.User.findAll({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+
+    return res.send(
+      JSON.stringify({
+        status: true,
+        notice: "Success",
+        data: user,
+      })
+    );
+  }
+
+  async editUser(req, res) {
+    await userService.editUser(req.body, req.params.id).then((data) => {
+      return res.status(200).json({
+        status: true,
+        notice: "Sua thanh cong",
+        data: data,
+      });
+    });
+  }
+
+  async login(req, res) {
+    // check email exist
+    // compare pw
+    // return userInfo
+    // access token
+    let email = req.body.email;
+    let pw = req.body.password;
+
+    if (!email || !pw) {
+      return res.status(500).json({
+        status: false,
+        message: "Missing inputs parameter",
+      });
     }
 
-    async login(req, res) {
-
-        // check email exist
-        // compare pw
-        // return userInfo
-        // access token
-        let email = req.body.email;
-        let pw = req.body.password;
-
-        if (!email || !pw) {
-            return res.status(500).json({
-                status: false,
-                message: "Missing inputs parameter"
-            });
+    await userService.checkIsExistUser(req.body).then(async (result) => {
+      if (result) {
+        function callback(msg) {
+          return res.status(200).json({
+            status: false,
+            message: msg,
+          });
         }
 
-        await userService.checkIsExistUser(req.body).then(async (result) => {
+        await userService
+          .handleLogin(req.body, result, callback)
+          .then((result) => {
             if (result) {
-                function callback(msg) {
-                    return res.status(200).json({
-                        status: false,
-                        message: msg,
-                    });
-                }
+              delete result.password;
 
-                await userService.handleLogin(req.body, result, callback).then((result) => {
-                    if (result) {
-                        delete result.password;
-
-                        return res.status(200).json({
-                            status: true,
-                            message: "Success",
-                            data: result
-                        });
-                    }
-                });
-            }
-
-            return res.status(200).json({
+              return res.status(200).json({
                 status: true,
-                message: "Fail",
-                data: "Error email"
-            });
-        });
-    }
+                message: "Success",
+                data: result,
+              });
+            }
+          });
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "Fail",
+        data: "Error email",
+      });
+    });
+  }
 }
 
-module.exports = new UserController;
+module.exports = new UserController();
